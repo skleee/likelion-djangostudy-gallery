83985d5d5974d9e5e1e61d162237b2a518ac86fa
SKLee <leesk1027@gmail.com>
2019-03-07 06:24:07 +0900

    settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -1,5 +1,4 @@
-import os, json
-from django.core.exceptions import ImproperlyConfigured
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -9,21 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-SECRET_KEY = get_secret("SECRET_KEY")
+# SECRET_KEY = 'qngl0yrn3iz1tld-2wo!@a@3ff7$n6l-()p-hw#73$0yxsg5ou'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'qngl0yrn3iz1tld-2wo!@a@3ff7$n6l-()p-hw#73$0yxsg5ou')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
